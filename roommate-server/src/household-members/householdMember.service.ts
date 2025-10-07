9debe9eb1fe0e7f26279290b7fab683dc416e8cc
souravseal99 <sourav.seal1999@gmail.com>
2025-10-07 16:23:18 +0530

    added get household members by household id service

--- roommate-server/src/household-members/householdMember.service.ts
+++ roommate-server/src/household-members/householdMember.service.ts
@@ -3,6 +3,7 @@ import { Role } from "@generated/prisma";
 import { HouseholdMemberDto } from "@common/dtos/HouseholdMemberDto";
 import { HouseholdMemberRepo } from "@src/household-members/householdMember.repo";
 import { StatusCodes } from "http-status-codes";
+import { HouseholdRepository } from "@src/households/household.repo";
 
 export class HouseholdMemberService {
   static async create(userId: string, householdId?: string, role?: Role) {
@@ -26,6 +27,30 @@ export class HouseholdMemberService {
         householdMember: createdHouseholdMember,
       },
       "Successfully created household member"
+    );
+  }
+
+  static async getHouseholdMembersByHouseholdId(householdId: string) {
+    const household = await HouseholdRepository.getHouseholdById(householdId);
+
+    if (!household) {
+      return ApiResponse.error("Household not found", StatusCodes.NOT_FOUND);
+    }
+
+    const householdMembers = await HouseholdMemberRepo.getByHouseholdId(
+      householdId
+    );
+
+    if (!householdMembers) {
+      return ApiResponse.error(
+        "No members found for this household",
+        StatusCodes.NOT_FOUND
+      );
+    }
+
+    return ApiResponse.success(
+      householdMembers,
+      "Successfully fetched household members"
     );
   }
 }
