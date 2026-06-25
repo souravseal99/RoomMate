b988e873961b1af4b0c1b3a33846d7d92ba36bda
Sourav Seal <69809086+souravseal99@users.noreply.github.com>
2026-06-26 02:01:02 +0530

    Develop (#139)
    * feat: (134) updated pr_quality_checks.yaml (#136)
    
    * Nightly changes (#138)
    
    * feat: (134) updated pr_quality_checks.yaml
    
    * feat: (134) updated eslint configuration
    
    * feat: (134) fixing linting issues

--- roommate-server/src/common/errors/ApiError.ts
+++ roommate-server/src/common/errors/ApiError.ts
@@ -1,11 +1,11 @@
 export class ApiError extends Error {
   public statusCode: number;
-  public errors: { field: String; message: String }[] | undefined;
+  public errors: { field: string; message: string }[] | undefined;
 
   constructor(
     statusCode: number,
     message: string,
-    errors?: { field: String; message: String }[]
+    errors?: { field: string; message: string }[]
   ) {
     super(message);
     this.statusCode = statusCode;
