5db9c5d3a0ec81db794c6ea8d5ac755b35a567bd
Sourav Seal <sourav.seal1999@gmail.com>
2026-09-05 23:23:12 +0530

    fix: (deploy) fixed env consumption issue

--- roommate-app/src/api/config.ts
+++ roommate-app/src/api/config.ts
@@ -1,2 +1,2 @@
 import { env } from '@/utils/utils';
-export const SERVER_BASE_URL = env('SERVER_BASE_URL') || 'http://127.0.0.1:5000';
+export const SERVER_BASE_URL = env('VITE_SERVER_BASE_URL') || 'http://127.0.0.1:5000';
